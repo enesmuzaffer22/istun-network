7865f23fa662900f02947edbaa3f3ff7f4f4b81e
Muzaffer Enes <77478332+enesmuzaffer22@users.noreply.github.com>
2025-07-14 10:43:55 +0300

    feat: added job section for landing page with animations.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,12 +3,14 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AboutUsSection from "../components/AboutUsSection";
 import Footer from "../components/Footer";
+import JobSection from "../components/JobSection";
 
 function LandingPage() {
   return (
     <div>
       <Hero />
       <AboutUsSection />
+      <JobSection />
     </div>
   );
 }
